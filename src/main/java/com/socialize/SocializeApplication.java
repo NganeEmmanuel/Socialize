d003398a95f3d416345fdc429ad5c39faf77a5b4
NganeEmmanuel <emmanuelngane06@gmail.com>
2024-05-16 13:52:09 +0100

    re-initializing the frontend to use js \n installing axios and setting up different folders for components, utilities, and services \n setting up a test rest api for connecting frontend and backend

--- src/main/java/com/socialize/SocializeApplication.java
+++ src/main/java/com/socialize/SocializeApplication.java
@@ -1,9 +1,12 @@
 package com.socialize;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class SocializeApplication {
 
     public static void main(String[] args) {
