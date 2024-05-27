a3e9f810812ff5e7a16160f110ad34dd9ff915ac
KING1ADB <kingadb181@gmail.com>
2024-05-27 06:38:22 -0700

    signin/sign-up page

--- src/main/frontend/socialize/src/components/signin/Login.jsx
+++ src/main/frontend/socialize/src/components/signin/Login.jsx
@@ -1,81 +1,117 @@
-import React, {useEffect, useState} from 'react';
-import {getHome} from "../utils/ApiFunctions.js";
-import topLeftImage from "./top-left-image.jpg";
-import topRightImage from "./top-right-image.jpg";
-import "./styles.css";
+import React, { useEffect, useState } from 'react';
+import { getHome } from "../utils/ApiFunctions.js";
+import * as Components from './Components.jsx';
+import './stule.css';
+import './styles.css'
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
+import image3 from './images/image3.jpg';
+import image4 from './images/image4.jpg';
+import image5 from './images/image5.jpg';
+import image6 from './images/image6.jpg';
+import image7 from './images/image7.jpg';
+import image8 from './images/image8.jpg';
+import image9 from './images/image9.jpg';
+import image10 from './images/image10.jpg';
+import image11 from './images/image11.jpg';
+import image12 from './images/image12.jpg';
+import image13 from './images/image13.jpg';
+import image14 from './images/image14.jpg';
+import image15 from './images/image15.jpg';
 
-const Login = (props) => {
-    const  [newHome, setNewHome] = useState("")
+const App = () => {
+    const [signIn, toggle] = React.useState(true);
+    const [newHome, setNewHome] = useState("");
+
+    const images = [
+        image1,
+        image2,
+        image3,
+        image4,
+        image5,
+        image6,
+        image7,
+        image8,
+        image9,
+        image10,
+        image11,
+        image12,
+        image13,
+        image14,
+        image15,
+    ];
+
     useEffect(() => {
         getHome().then((data) => {
-            setNewHome(data)
-        })
+            setNewHome(data);
+        });
     }, []);
 
-
-    const [name, setName] = useState("");
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Handle form submission here
-      };
-
     return (
-        <><div className="signup-page">
-            <img src={topLeftImage} alt="Top Left Image" className="top-left-image" />
-            <img src={topRightImage} alt="Top Right Image" className="top-right-image" />
-            <div className="signup-form">
-                <h2>Create a new account</h2>
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label htmlFor="name">Name:</label>
-                        <input
-                            type="text"
-                            id="name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            required />
-                    </div>
-                    <div>
-                        <label htmlFor="username">Username:</label>
-                        <input
-                            type="text"
-                            id="username"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            required />
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email:</label>
-                        <input
-                            type="email"
-                            id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password:</label>
-                        <input
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required />
-                    </div>
-                    <button type="submit">Sign Up</button>
-                </form>
-                <p>Already have an account? <a href="#">Log in</a></p>
+        <>
+            <Components.Container>
+                <Components.SignUpContainer signinIn={signIn}>
+                    <Components.Form>
+                        <Components.Title>Create Account</Components.Title>
+                        <Components.Input type='text' placeholder='Full Name' />
+                        <Components.Input type='text' placeholder='UserName' />
+                        <Components.Input type='email' placeholder='Email' />
+                        <Components.Input type='password' placeholder='Password' />
+                        <Components.Button>Sign Up</Components.Button>
+                    </Components.Form>
+                </Components.SignUpContainer>
+
+                <Components.SignInContainer signinIn={signIn}>
+                    <Components.Form>
+                        <Components.Title>Sign in</Components.Title>
+                        <Components.Input type='text' placeholder='Username' />
+                        <Components.Input type='password' placeholder='Password' />
+                        <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                        <Components.Button>Sigin In</Components.Button>
+                    </Components.Form>
+                </Components.SignInContainer>
+
+                <Components.OverlayContainer signinIn={signIn}>
+                    <Components.Overlay signinIn={signIn}>
+                        <Components.LeftOverlayPanel signinIn={signIn}>
+                            <Components.Title>Welcome Back!</Components.Title>
+                            <Components.Paragraph>
+                                To keep connected with us please login with your personal info
+                            </Components.Paragraph>
+                            <Components.GhostButton onClick={() => toggle(true)}>
+                                Sign In
+                            </Components.GhostButton>
+                        </Components.LeftOverlayPanel>
+
+                        <Components.RightOverlayPanel signinIn={signIn}>
+                            <Components.Title>Hello, Friend!</Components.Title>
+                            <Components.Paragraph>
+                                Enter Your personal details and start journey with us
+                            </Components.Paragraph>
+                            <Components.GhostButton onClick={() => toggle(false)}>
+                                Sigin Up
+                            </Components.GhostButton>
+                        </Components.RightOverlayPanel>
+                    </Components.Overlay>
+                </Components.OverlayContainer>
+            </Components.Container>
+
+            <div className="image-container">
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        className="image"
+                        src={image}
+                        alt={`Inclined Image ${index + 1}`}
+                    />
+                ))}
             </div>
-        </div>
-        <div>
+
+            <div>
                 <h1>{newHome}</h1>
             </div>
-    </>
+        </>
     );
-}
+};
 
-export default Login;
+export default App;
