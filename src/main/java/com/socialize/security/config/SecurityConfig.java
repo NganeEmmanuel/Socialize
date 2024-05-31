1971e33fa87c48abb2dc28cfaffcf16c331eeef3
Ngane Emmanuel <97377179+NganeEmmanuel@users.noreply.github.com>
2024-05-31 20:11:46 +0100

    Update SecurityConfig.java

--- src/main/java/com/socialize/security/config/SecurityConfig.java
+++ src/main/java/com/socialize/security/config/SecurityConfig.java
@@ -25,7 +25,6 @@ public class SecurityConfig  {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-
                         .requestMatchers("/api/v1/auth/**") //todo please when you add your endpoints here for testing, please remove them and allow just the default before you push and pull request
                         .permitAll()
                         .anyRequest().authenticated()
