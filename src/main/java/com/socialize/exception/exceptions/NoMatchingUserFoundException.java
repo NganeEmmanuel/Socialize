101704d646cace16678243f2589cfc72a542f1a0
Ngane Emmanuel <97377179+NganeEmmanuel@users.noreply.github.com>
2024-06-09 11:03:11 +0100

    Update NoMatchingUserFoundException.java
    to match master

--- src/main/java/com/socialize/exception/exceptions/NoMatchingUserFoundException.java
+++ src/main/java/com/socialize/exception/exceptions/NoMatchingUserFoundException.java
@@ -1,22 +1,10 @@
 package com.socialize.exception.exceptions;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class NoMatchingUserFoundException extends RuntimeException {
+public class NoMatchingUserFoundException extends RuntimeException{
 
     public NoMatchingUserFoundException(String username) {
         super("No user found with username and/or name matching {} " + username);
-    }
-
-    @SpringBootApplication
-    public class Application {
-        public static void main(String[] args) {
-            SpringApplication.run(Application.class, args);
-        }
     }
 
 }
