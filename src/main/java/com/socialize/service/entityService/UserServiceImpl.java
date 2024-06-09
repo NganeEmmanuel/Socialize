a029bc8e9d226205f4f125a627ce62181dc1f53c
Ngane Emmanuel <97377179+NganeEmmanuel@users.noreply.github.com>
2024-06-09 11:41:51 +0100

    Update UserServiceImpl.java
    added todo for @Martine-Carole

--- src/main/java/com/socialize/service/entityService/UserServiceImpl.java
+++ src/main/java/com/socialize/service/entityService/UserServiceImpl.java
@@ -80,11 +80,14 @@ public class UserServiceImpl implements UserService {
 //        return userMapper.mapToDTOList(users);
         return null;
     }
+
+    //Todo @Carole please extract this code into its own class [UserProfileServiceImpl].
+    //Todo @Carole make sure to create a custome exception [UserProfileNotFoundException] and throw that instead of the basic exception class. work with brian and dilan on it
     @Override
     public UserDTO getUserDTOById(Long userId) throws Exception {
         User user;
         try {
-             user = userRepository.findById(userId).orElseThrow(() -> new Exception("User not found"));
+             user = userRepository.findById(userId).orElseThrow(() -> new Exception("User not found"));  //todo throw custom exception
            
         } catch (Exception ex) {
             logger.error("An unexpected error occurred while retrieving user with ID: {}", userId, ex);
