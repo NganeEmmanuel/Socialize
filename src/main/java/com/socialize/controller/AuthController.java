7456f99b5936ca1c31cc75d408bacd9eb6338753
NganeEmmanuel <emmanuelngane06@gmail.com>
2024-06-12 23:37:36 +0100

    update request type top post mapping for login endpoint

--- src/main/java/com/socialize/controller/AuthController.java
+++ src/main/java/com/socialize/controller/AuthController.java
@@ -40,9 +40,11 @@ public class AuthController {
         return ResponseEntity.ok(authService.signup(request));
     }
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request){
         return ResponseEntity.ok(authService.login(request));
     }
+
+
 
 }
