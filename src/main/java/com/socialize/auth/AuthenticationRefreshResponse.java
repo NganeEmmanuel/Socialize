79297acaa3aac4112887fbb96ab673c59d3e0a8c
NganeEmmanuel <emmanuelngane06@gmail.com>
2024-06-13 02:38:45 +0100

    removing git ignore on apllication classe to push changes

---
+++ src/main/java/com/socialize/auth/AuthenticationRefreshResponse.java
@@ -0,0 +1,14 @@
+package com.socialize.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRefreshResponse {
+    private String token;
+}
